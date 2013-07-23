ed6dca7ec48eb328efbed3697f96d0005b70107a
panfayang <panfayang@gmail.com>
2013-07-23 11:28:12 -0400

    Create biggerInt.py

---
+++ biggerInt.py
@@ -0,0 +1,20 @@
+def biggerInt():
+  number = raw_input("Please enter a positive integer")
+	
+	while True:
+		try:
+			int(number)
+			break
+		except ValueError:
+			number = raw_input("Please enter a positive integer")
+
+	li = []
+	final = ""
+	for i in str(number):
+		li.append(int(i))
+	li.sort()
+	for i in range(len(li)-1,-1,-1):
+		final+=str(li[i])
+	return final
+
+print biggerInt()
